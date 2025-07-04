b8a5726046fe0c764656e9f2a38be0e9d9e5a1e8
kpobb1989 <kpobb1989@gmail.com>
2025-07-04 12:15:29 -0700

    init

---
+++ backend/LeetCodeDotNetFormatterApi/Program.cs
@@ -0,0 +1,39 @@
+// Program.cs
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin() // For development, allow any origin
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(); // Use the CORS policy
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
